6f7813c1ee75212b4dda56954ee1cb788cebabef
HALBY <develop@halby24.com>
2023-09-15 13:05:44 +0900

    wip

--- blender_plugin/fbx_exporter/construct_export_object.py
+++ blender_plugin/fbx_exporter/construct_export_object.py
@@ -2,12 +2,19 @@
 # This software is released under the MIT License, see LICENSE.
 
 import bpy
-import ctypes
+from .c_data_structure import *
 
 class ConstructExportObject:
     def __init__(self, objs: [bpy.types.Object]) -> None:
         self.objs = objs
     
-    
-    def getExportData(self) -> bytes:
-        pass
+    def getExportData(self) -> ExportData:
+        export_data = ExportData()
+        for obj in self.objs:
+            export_data.objects.append(self.getExportObject(obj))
+
+    @staticmethod
+    def getExportObjFromBlenderObj(obj: bpy.types.Object) -> ObjectData:
+        obj_data = ObjectData()
+        obj_data.name = obj.name
+        
