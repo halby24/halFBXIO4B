2811acee376fbafdace0276aa4890dda928bd0de
HALBY <develop@halby24.com>
2023-09-21 21:07:04 +0900

    wip

--- exporter/cmake/FindFbxsdk.cmake
+++ exporter/cmake/FindFbxsdk.cmake
@@ -1,12 +1,21 @@
-if (not defined FBXSDK_VERSION)
+if(NOT DEFINED FBXSDK_VERSION)
     set(FBXSDK_VERSION "2020.3.4")
 endif()
-if (WIN32 && not defined FBXSDK_ROOT)
+if(WIN32 AND NOT DEFINED FBXSDK_ROOT)
     set(FBXSDK_ROOT "C:/Program Files/Autodesk/FBX/FBX SDK/${FBXSDK_VERSION}")
 endif()
-if (APPLE && not defined FBXSDK_ROOT)
+if(APPLE AND NOT DEFINED FBXSDK_ROOT)
     set(FBXSDK_ROOT "/Applications/Autodesk/FBX SDK/${FBXSDK_VERSION}")
 endif()
+
+if(WIN32)
+    set(LIB_EXT ".lib")
+elseif(APPLE)
+    set(LIB_EXT ".dylib")
+else()
+    set(LIB_EXT ".so")
+endif()
+set(FBXSDK_LIB "libfbxsdk${LIB_EXT} libxml${LIB_EXT} zlib${LIB_EXT}")
 
 if (WIN32)
     IF(MSVC_VERSION GREATER 1910 AND MSVC_VERSION LESS 1920)
@@ -28,6 +37,7 @@ find_path(FBXSDK_INCLUDE_DIR fbxsdk.h
         ${FBXSDK_ROOT}
         /usr
         /usr/local
+        ~/.local
     PATH_SUFFIXES
         include
 )
@@ -43,6 +53,7 @@ find_library(FBXSDK_LIBRARY
         ${FBXSDK_ROOT}
         /usr
         /usr/local
+        ~/.local
     PATH_SUFFIXES
         ${FBXSDK_LIB_SUFFIX}
 )
