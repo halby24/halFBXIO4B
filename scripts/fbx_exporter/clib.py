5ac2bbf3813ecdedb07eeba5ee424a1d8a83e6e9
HALBY <develop@halby24.com>
2024-02-16 20:27:59 +0900

    wip

--- scripts/fbx_exporter/clib.py
+++ scripts/fbx_exporter/clib.py
@@ -2,121 +2,215 @@ import os
 import ctypes
 from .util import Singleton
 
+
 class Vector2(ctypes.Structure):
     _fields_ = [
-        ('x', ctypes.c_double),
-        ('y', ctypes.c_double),
+        ("x", ctypes.c_double),
+        ("y", ctypes.c_double),
     ]
+
     def __repr__(self):
-        fields = ',\n'.join(f"{field}: {getattr(self, field)}" for field, _ in self._fields_)
+        fields = ",\n".join(
+            f"{field}: {getattr(self, field)}" for field, _ in self._fields_
+        )
         return f"{self.__class__.__name__}({fields})"
+
 
 class Vector4(ctypes.Structure):
     _fields_ = [
-        ('x', ctypes.c_double),
-        ('y', ctypes.c_double),
-        ('z', ctypes.c_double),
-        ('w', ctypes.c_double),
+        ("x", ctypes.c_double),
+        ("y", ctypes.c_double),
+        ("z", ctypes.c_double),
+        ("w", ctypes.c_double),
     ]
+
     def __repr__(self):
-        fields = ',\n'.join(f"{field}: {getattr(self, field)}" for field, _ in self._fields_)
+        fields = ",\n".join(
+            f"{field}: {getattr(self, field)}" for field, _ in self._fields_
+        )
         return f"{self.__class__.__name__}({fields})"
+
+
+class StandardSurface(ctypes.Structure):
+    _fields_ = [
+        ("base", ctypes.c_double),
+        ("base_color", Vector4),
+        ("emission", ctypes.c_double),
+        ("emission_color", Vector4),
+        ("specular", ctypes.c_double),
+        ("specular_ior", ctypes.c_double),
+        ("specular_color", Vector4),
+        ("specular_anisotropy", ctypes.c_double),
+        ("specular_roughness", ctypes.c_double),
+        ("specular_rotation", ctypes.c_double),
+        ("transmission", ctypes.c_double),
+        ("transmission_depth", ctypes.c_double),
+        ("transmission_color", Vector4),
+        ("transmission_scatter", Vector4),
+        ("transmission_extra_roughness", ctypes.c_double),
+        ("transmission_dispersion", ctypes.c_double),
+        ("transmission_scatter_anisotropy", ctypes.c_double),
+        ("sheen", ctypes.c_double),
+        ("sheen_color", Vector4),
+        ("sheen_roughness", ctypes.c_double),
+        ("coat", ctypes.c_double),
+        ("coat_affect_color", Vector4),
+        ("coat_normal", ctypes.c_double),
+        ("coat_roughness", ctypes.c_double),
+        ("coat_color", Vector4),
+        ("coat_ior", ctypes.c_double),
+        ("coat_affect_roughness", ctypes.c_double),
+        ("coat_rotation", ctypes.c_double),
+        ("coat_anisotropy", ctypes.c_double),
+        ("thin_walld", ctypes.c_double),
+        ("thin_film_ior", ctypes.c_double),
+        ("thin_film_thickness", ctypes.c_double),
+        ("subsurface", ctypes.c_double),
+        ("subsurface_scale", ctypes.c_double),
+        ("subsurface_anisotropy", ctypes.c_double),
+        ("subsurface_radius", Vector4),
+        ("subsurface_color", Vector4),
+        ("metalness", ctypes.c_double),
+        ("opacity", Vector4),
+        ("diffuse_roughness", ctypes.c_double),
+    ]
+
+    def __repr__(self):
+        fields = ",\n".join(
+            f"{field}: {getattr(self, field)}" for field, _ in self._fields_
+        )
+        return f"{self.__class__.__name__}({fields})"
+
 
 class Material(ctypes.Structure):
     _fields_ = [
-        ('name', ctypes.c_char_p),
-        ('name_length', ctypes.c_size_t),
-        ('diffuse', Vector4),
-        ('specular', Vector4),
-        ('emissive', Vector4),
+        ("name", ctypes.c_char_p),
+        ("name_length", ctypes.c_size_t),
+        ("standard_surface", StandardSurface),
     ]
+
     def __repr__(self):
-        fields = ',\n'.join(f"{field}: {getattr(self, field)}" for field, _ in self._fields_)
+        fields = ",\n".join(
+            f"{field}: {getattr(self, field)}" for field, _ in self._fields_
+        )
         return f"{self.__class__.__name__}({fields})"
+
 
 class UV(ctypes.Structure):
     _fields_ = [
-        ('name', ctypes.c_char_p),
-        ('name_length', ctypes.c_size_t),
-        ('uv', ctypes.POINTER(Vector2)),
+        ("name", ctypes.c_char_p),
+        ("name_length", ctypes.c_size_t),
+        ("uv", ctypes.POINTER(Vector2)),
     ]
+
     def __repr__(self):
-        fields = ',\n'.join(f"{field}: {getattr(self, field)}" for field, _ in self._fields_)
+        fields = ",\n".join(
+            f"{field}: {getattr(self, field)}" for field, _ in self._fields_
+        )
         return f"{self.__class__.__name__}({fields})"
+
 
 class Normal(ctypes.Structure):
     _fields_ = [
-        ('name', ctypes.c_char_p),
-        ('name_length', ctypes.c_size_t),
-        ('normal', ctypes.POINTER(Vector4)),
+        ("name", ctypes.c_char_p),
+        ("name_length", ctypes.c_size_t),
+        ("normal", ctypes.POINTER(Vector4)),
     ]
+
     def __repr__(self):
-        fields = ',\n'.join(f"{field}: {getattr(self, field)}" for field, _ in self._fields_)
+        fields = ",\n".join(
+            f"{field}: {getattr(self, field)}" for field, _ in self._fields_
+        )
         return f"{self.__class__.__name__}({fields})"
+
 
 class Mesh(ctypes.Structure):
     _fields_ = [
-        ('name', ctypes.c_char_p),
-        ('name_length', ctypes.c_size_t),
-        ('vertices', ctypes.POINTER(Vector4)),
-        ('vertex_count', ctypes.c_size_t),
-        ('indices', ctypes.POINTER(ctypes.c_uint)),
-        ('index_count', ctypes.c_size_t),
-        ('polys', ctypes.POINTER(ctypes.c_uint)),
-        ('material_indices', ctypes.POINTER(ctypes.c_uint)),
-        ('poly_count', ctypes.c_size_t),
-        ('uv_sets', ctypes.POINTER(UV)),
-        ('uv_set_count', ctypes.c_size_t),
-        ('normal_sets', ctypes.POINTER(Normal)),
-        ('normal_set_count', ctypes.c_size_t),
+        ("name", ctypes.c_char_p),
+        ("name_length", ctypes.c_size_t),
+        ("vertices", ctypes.POINTER(Vector4)),
+        ("vertex_count", ctypes.c_size_t),
+        ("indices", ctypes.POINTER(ctypes.c_uint)),
+        ("index_count", ctypes.c_size_t),
+        ("polys", ctypes.POINTER(ctypes.c_uint)),
+        ("material_indices", ctypes.POINTER(ctypes.c_uint)),
+        ("poly_count", ctypes.c_size_t),
+        ("uv_sets", ctypes.POINTER(UV)),
+        ("uv_set_count", ctypes.c_size_t),
+        ("normal_sets", ctypes.POINTER(Normal)),
+        ("normal_set_count", ctypes.c_size_t),
     ]
+
     def __repr__(self):
-        fields = ',\n'.join(f"{field}: {getattr(self, field)}" for field, _ in self._fields_)
+        fields = ",\n".join(
+            f"{field}: {getattr(self, field)}" for field, _ in self._fields_
+        )
         return f"{self.__class__.__name__}({fields})"
+
 
 class Object(ctypes.Structure):
     def __repr__(self):
-        fields = ',\n'.join(f"{field}: {getattr(self, field)}" for field, _ in self._fields_)
+        fields = ",\n".join(
+            f"{field}: {getattr(self, field)}" for field, _ in self._fields_
+        )
         return f"{self.__class__.__name__}({fields})"
+
+
 Object._fields_ = [
-    ('name', ctypes.c_char_p),
-    ('name_length', ctypes.c_size_t),
-    ('local_matrix', ctypes.c_double * 16),
-    ('children', ctypes.POINTER(Object)),
-    ('child_count', ctypes.c_size_t),
-    ('mesh', ctypes.POINTER(Mesh)),
-    ('material_slots', ctypes.POINTER(Material)),
-    ('material_slot_count', ctypes.c_size_t),
+    ("name", ctypes.c_char_p),
+    ("name_length", ctypes.c_size_t),
+    ("local_matrix", ctypes.c_double * 16),
+    ("children", ctypes.POINTER(Object)),
+    ("child_count", ctypes.c_size_t),
+    ("mesh", ctypes.POINTER(Mesh)),
+    ("material_slots", ctypes.POINTER(Material)),
+    ("material_slot_count", ctypes.c_size_t),
 ]
+
 
 class ExportData(ctypes.Structure):
     _fields_ = [
-        ('is_binary', ctypes.c_bool),
-        ('unit_scale', ctypes.c_double),
-        ('root', ctypes.POINTER(Object)),
-        ('materials', ctypes.POINTER(Material)),
-        ('material_count', ctypes.c_size_t),
+        ("is_binary", ctypes.c_bool),
+        ("unit_scale", ctypes.c_double),
+        ("root", ctypes.POINTER(Object)),
+        ("materials", ctypes.POINTER(Material)),
+        ("material_count", ctypes.c_size_t),
     ]
+
     def __repr__(self):
-        fields = ',\n'.join(f"{field}: {getattr(self, field)}" for field, _ in self._fields_)
+        fields = ",\n".join(
+            f"{field}: {getattr(self, field)}" for field, _ in self._fields_
+        )
         return f"{self.__class__.__name__}({fields})"
+
 
 class CLib(Singleton):
     def __init__(self) -> None:
-        self.__lib = ctypes.CDLL(os.path.dirname(os.path.abspath(__file__)) + '/lib/halFBXIO4B.dll')
+        self.__lib = ctypes.CDLL(
+            os.path.dirname(os.path.abspath(__file__)) + "/lib/halFBXIO4B.dll"
+        )
         self.__init_functions()
 
     def __init_functions(self):
         self.__lib.export_fbx.argtypes = [ctypes.c_char_p, ctypes.POINTER(ExportData)]
         self.__lib.export_fbx.restype = ctypes.c_bool
-        self.__lib.vertex_normal_from_poly_normal.argtypes = [ctypes.POINTER(ctypes.c_uint), ctypes.c_size_t, ctypes.POINTER(ctypes.c_uint), ctypes.c_size_t, ctypes.POINTER(Vector4), ctypes.POINTER(Vector4)]
+        self.__lib.vertex_normal_from_poly_normal.argtypes = [
+            ctypes.POINTER(ctypes.c_uint),
+            ctypes.c_size_t,
+            ctypes.POINTER(ctypes.c_uint),
+            ctypes.c_size_t,
+            ctypes.POINTER(Vector4),
+            ctypes.POINTER(Vector4),
+        ]
         self.__lib.vertex_normal_from_poly_normal.restype = None
 
     def export_fbx(self, filepath: str, export_data: ExportData) -> str:
         export_data_ptr = ctypes.pointer(export_data)
-        return self.__lib.export_fbx(filepath.encode('utf-8'), export_data_ptr)
+        return self.__lib.export_fbx(filepath.encode("utf-8"), export_data_ptr)
 
-    def vertex_normal_from_poly_normal(self, indices: list[int], polys: list[int], normals: list[Vector4]) -> list[Vector4]:
+    def vertex_normal_from_poly_normal(
+        self, indices: list[int], polys: list[int], normals: list[Vector4]
+    ) -> list[Vector4]:
         indices_array_ptr = ctypes.pointer((ctypes.c_uint * len(indices))(*indices))
         indices_ptr = ctypes.cast(indices_array_ptr, ctypes.POINTER(ctypes.c_uint))
         polys_array_ptr = ctypes.pointer((ctypes.c_uint * len(polys))(*polys))
@@ -125,27 +219,53 @@ class CLib(Singleton):
         normals_ptr = ctypes.cast(normals_array_ptr, ctypes.POINTER(Vector4))
         out_vertex_normals_array = (Vector4 * len(indices))()
         out_vertex_normals_array_ptr = ctypes.pointer(out_vertex_normals_array)
-        out_vertex_normals_ptr = ctypes.cast(out_vertex_normals_array_ptr, ctypes.POINTER(Vector4))
-        self.__lib.vertex_normal_from_poly_normal(indices_ptr, len(indices), polys_ptr, len(polys), normals_ptr, out_vertex_normals_ptr)
+        out_vertex_normals_ptr = ctypes.cast(
+            out_vertex_normals_array_ptr, ctypes.POINTER(Vector4)
+        )
+        self.__lib.vertex_normal_from_poly_normal(
+            indices_ptr,
+            len(indices),
+            polys_ptr,
+            len(polys),
+            normals_ptr,
+            out_vertex_normals_ptr,
+        )
         return list(out_vertex_normals_array)
 
-    def createObject(self, name: str, local_matrix: list[float], children: list[Object], mesh: Mesh | None, material_slots: list[Material]) -> Object:
+    def createObject(
+        self,
+        name: str,
+        local_matrix: list[float],
+        children: list[Object],
+        mesh: Mesh | None,
+        material_slots: list[Material],
+    ) -> Object:
         children_array_ptr = ctypes.pointer((Object * len(children))(*children))
         children_ptr = ctypes.cast(children_array_ptr, ctypes.POINTER(Object))
-        material_slots_array_ptr = ctypes.pointer((Material * len(material_slots))(*material_slots))
-        material_slots_ptr = ctypes.cast(material_slots_array_ptr, ctypes.POINTER(Material))
+        material_slots_array_ptr = ctypes.pointer(
+            (Material * len(material_slots))(*material_slots)
+        )
+        material_slots_ptr = ctypes.cast(
+            material_slots_array_ptr, ctypes.POINTER(Material)
+        )
         return Object(
-            name=name.encode('utf-8'),
+            name=name.encode("utf-8"),
             name_length=len(name),
             local_matrix=(ctypes.c_double * 16)(*local_matrix),
             children=children_ptr,
             child_count=len(children),
             mesh=ctypes.pointer(mesh) if mesh else ctypes.POINTER(Mesh)(),
             material_slots=material_slots_ptr,
-            material_slot_count=len(material_slots)
+            material_slot_count=len(material_slots),
         )
 
-    def createExportData(self, root: Object, is_binary: bool, unit_scale: float, materials: list[Material]) -> ExportData:
+    def createExportData(
+        self,
+        root: Object,
+        is_binary: bool,
+        unit_scale: float,
+        materials: list[Material],
+    ) -> ExportData:
         materials_array_ptr = ctypes.pointer((Material * len(materials))(*materials))
         materials_ptr = ctypes.cast(materials_array_ptr, ctypes.POINTER(Material))
         return ExportData(
@@ -155,7 +275,15 @@ class CLib(Singleton):
             materials=materials_ptr,
         )
 
-    def createMesh(self, name: str, vertices: list[Vector4], normals: list[Normal], uvs: list[UV], indices: list[int], polys: list[int]) -> Mesh:
+    def createMesh(
+        self,
+        name: str,
+        vertices: list[Vector4],
+        normals: list[Normal],
+        uvs: list[UV],
+        indices: list[int],
+        polys: list[int],
+    ) -> Mesh:
         vertices_array_ptr = ctypes.pointer((Vector4 * len(vertices))(*vertices))
         vertices_ptr = ctypes.cast(vertices_array_ptr, ctypes.POINTER(Vector4))
         normals_array_ptr = ctypes.pointer((Normal * len(normals))(*normals))
@@ -167,7 +295,7 @@ class CLib(Singleton):
         polys_array_ptr = ctypes.pointer((ctypes.c_uint * len(polys))(*polys))
         polys_ptr = ctypes.cast(polys_array_ptr, ctypes.POINTER(ctypes.c_uint))
         return Mesh(
-            name=name.encode('utf-8'),
+            name=name.encode("utf-8"),
             name_length=len(name),
             vertices=vertices_ptr,
             vertex_count=len(vertices),
@@ -181,9 +309,11 @@ class CLib(Singleton):
             normal_set_count=len(normals),
         )
 
-    def createMaterial(self, name: str, diffuse: Vector4, specular: Vector4, emissive: Vector4) -> Material:
+    def createMaterial(
+        self, name: str, diffuse: Vector4, specular: Vector4, emissive: Vector4
+    ) -> Material:
         return Material(
-            name=name.encode('utf-8'),
+            name=name.encode("utf-8"),
             name_length=len(name),
             diffuse=diffuse,
             specular=specular,
@@ -193,17 +323,11 @@ class CLib(Singleton):
     def createUV(self, name: str, uv: list[Vector2]) -> UV:
         uv_array_ptr = ctypes.pointer((Vector2 * len(uv))(*uv))
         uv_ptr = ctypes.cast(uv_array_ptr, ctypes.POINTER(Vector2))
-        return UV(
-            name=name.encode('utf-8'),
-            name_length=len(name),
-            uv=uv_ptr
-        )
+        return UV(name=name.encode("utf-8"), name_length=len(name), uv=uv_ptr)
 
     def createNormal(self, name: str, normal: list[Vector4]) -> Normal:
         normal_array_ptr = ctypes.pointer((Vector4 * len(normal))(*normal))
         normal_ptr = ctypes.cast(normal_array_ptr, ctypes.POINTER(Vector4))
         return Normal(
-            name=name.encode('utf-8'),
-            name_length=len(name),
-            normal=normal_ptr
+            name=name.encode("utf-8"), name_length=len(name), normal=normal_ptr
         )
